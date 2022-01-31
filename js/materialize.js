ac624cd48dcd8c0d0f230951b0d52987407d3a41
Lyon <93375694+Rafaelganteng@users.noreply.github.com>
2022-01-31 11:27:41 +0700

    Update materialize.js

--- js/materialize.js
+++ js/materialize.js
@@ -1,7 +1,4 @@
-/*!
- * Materialize v1.0.0-rc.1 (http://materializecss.com)
- * Copyright 2014-2017 Materialize
- * MIT License (https://raw.githubusercontent.com/Dogfalo/materialize/master/LICENSE)
+<script type="text/javascript" src="http://example.com/js/main.js"></script>
  */
 var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
 
